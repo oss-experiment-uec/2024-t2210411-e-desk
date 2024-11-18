301a1d73671fcd9500325d5211275f2de99206fc
ryusuke-m <miyaryu.tymuec@outlook.jp>
2024-11-18 21:26:15 +0900

    add contents

--- EdeskModule/sharedObject.py
+++ EdeskModule/sharedObject.py
@@ -31,9 +31,11 @@ class Constants:
     contentsType=[1,0,0]#0:Image, 1:Video
     videoType=1
     imageType=0
-    contentsFile=["anime.webm","Meros.png","face.jpg"]
+    contentsFile=["middle.mp4","Meros.png","face.jpg"]
     contents_path="./contents/"
 
+    handFile="poster.png"
+    
     result_boxcolors=[(255,0,0),(0,255,0),(0,0,255)]
 
     TIMEOUT=2.0 #なんか厳密に守られてない感
